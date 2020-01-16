9b34ed7119248d079081e3fb82d5d8005ac69d54
Prashant Kumar <prashant30071992@gmail.com>
2020-01-16 20:53:26 +0530

    First commit

---
+++ src/main/java/com/example/demo2/controller/TrainingController.java
@@ -0,0 +1,13 @@
+package com.example.demo2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TrainingController {
+
+    @GetMapping ("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
